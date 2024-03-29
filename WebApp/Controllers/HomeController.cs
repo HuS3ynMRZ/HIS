23e2de9ac270f2eae2b274434271fcd2fb844712
HuseynMRZ <hsynmrz28@gmail.com>
2024-03-29 19:14:34 +0100

    Add project files.

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "this is my new site motherfuckers";
+        }
+
+        public string Error()
+        {
+            return "Houston, we have an error";
+        }
+    }
+}
